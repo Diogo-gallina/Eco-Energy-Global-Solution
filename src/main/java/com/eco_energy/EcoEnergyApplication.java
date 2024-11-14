a8382d26eb1b246be96b26427594e4ae10d82bd8
diogo-gallina <diogogallina@gmail.com>
2024-11-13 21:13:53 -0300

    feat: setup initial spring code

---
+++ src/main/java/com/eco_energy/EcoEnergyApplication.java
@@ -0,0 +1,13 @@
+package com.eco_energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoEnergyApplication.class, args);
+	}
+
+}
